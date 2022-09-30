448a1a38bf1ad75cb5a473f7bb21c42feed10e35
Douglas Ward <30300064+dougimus@users.noreply.github.com>
2022-09-30 09:32:03 -0400

    added example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
-#include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+// Mohawk College - COMP-10184 
+// 
+// Digital Input/Output Lab - Test Program 
+// 
+#include <Arduino.h> 
+ 
+void setup() { 
+  // configure the USB serial monitor 
+  Serial.begin(115200); 
+ 
+  // configure pin D5 as digital input - this is the pushbutton 
+  pinMode(D5, INPUT_PULLUP); 
+ 
+  // configure pin D4 as a digital output - this is the LED 
+  pinMode(D4, OUTPUT); 
+ 
+} 
+ 
+ 
+void loop() { 
+  int iButton; 
+ 
+  // read the push button input... 
+  iButton = digitalRead(D5); 
+  // ... and write it to the LED output 
+  digitalWrite(D4, iButton); 
+} 
